ea4bf21ea82ffb4d73e8125fcbecd3fbbd3adbd0
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-09-20 12:28:18 +0530

    delete rows from table
    here delete, drop and truncate usage is done
    delete removes row/rows, used with conditions, table remains, doesn't reset identity column, slower can be rolled back
    truncate removes all the rows at once , faster, no rollback possible, table remains, resets identity column, keeps structure
    drop, it removes entire table, no rollback, fastest,

---
+++ deleterowsfromtable.sql
@@ -0,0 +1,25 @@
+select * from dimproduct_bkup
+
+drop table dimproduct_bkup
+
+select * into dimprobkup from DimProduct
+
+select * from dimprobkup
+
+delete dimprobkup
+
+truncate table dimprobkup
+
+drop table dimprobkup
+
+delete from dimprobkup
+where Color='Red-hi' and listprice>=1000
+
+select * from employees8
+delete from employees8
+
+truncate table employees8
+
+
+delete from dimprobkup 
+where standardcost>=500 and Color='Red'
