ea0323dbf2d00528cd39bfbfca1483a346a23e12
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-09-23 14:10:14 +0530

    date time functions
    learnt datetime functions like, getdate(), sysdatetime(), sysutcdatetime(), datefromparts(), datetimefromparts(), year(), month(), day(), datepart(), datename(), format()

---
+++ datetimefunctionspart1.sql
@@ -0,0 +1,12 @@
+
+select getdate()
+
+select sysdatetime()
+
+select SYSUTCDATETIME()
+
+ select '2023-11-31'
+
+ select datefromparts(2023,12,31)
+
+ select datetimefromparts(2023,12,31,8,45,30,0)
---
+++ datetimefunctionspart2.sql
@@ -0,0 +1,37 @@
+select year(getdate())
+select month(getdate())
+select day(getdate())
+
+select Birthdate,year(Birthdate) from DimEmployee
+
+select Birthdate, month(Birthdate) from DimEmployee
+
+select Birthdate, day(Birthdate) from DimEmployee
+
+select datepart(year, getdate())
+
+select datepart(MONTH, getdate())
+
+select datepart(weekday, getdate())
+
+select datename(year, getdate())
+
+select datename(mm, getdate())
+
+select datename(weekday, getdate())
+
+select format (getdate(), 'yy')
+
+select format (getdate(), 'yyyy')
+
+select format(getdate(), 'MM')
+
+select format(getdate(), 'MMM')
+
+select format(getdate(), 'dd')
+
+select format(getdate(), 'ddd')
+
+select format(getdate(),'dd-MM-yyyy hh:mm:ss')
+
+select format(getdate(), 'dd-MM-yyyy HH:mm:ss')
