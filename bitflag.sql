e955bc2b5c8ad046d80810b88fd4cfe7a651cb06
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-09-25 23:20:01 +0530

    bit flag
    bit is a flag, contains 1 or 0, 'True' or 'False'

---
+++ bitflag.sql
@@ -0,0 +1,8 @@
+create table empbit(empid int, empname varchar(100), isactive bit)
+
+insert into empbit(empid, empname, isactive) values (1,'sonu',1)
+insert into empbit(empid, empname, isactive) values (2,'monu',0)
+insert into empbit(empid, empname, isactive) values (3,'sunil','True')
+insert into empbit(empid, empname, isactive) values (4,'amol','False')
+
+select * from empbit
