687eb3601580ace67d9451e610b94359943679b2
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-09-25 23:43:07 +0530

    data type conversion
    data type conversion, it tries to do implicit conversion, whenever applicable
    10 + 20 = 30
    10 + '20' = 30
    'ak' + 20 = error
    '10' + '20' = 1020
    cast and convert(tsql, mssqlserver only) are used to explicitly convert values
    select cast('10' as int)
    select cast('ListPrice' as money)
    select convert ( int , '10')
    select convert (money, ListPrice) from DimProduct

---
+++ datatypeconversion.sql
@@ -0,0 +1,13 @@
+select 10 + 20
+select 10 + '20'
+select '10'+'20'
+--select 'ak'+20
+
+select cast('10' as int)
+select cast(ListPrice as money) from DimProduct
+
+select convert(int, '10')
+
+select convert(money, ListPrice)
+from DimProduct
+
