7cfbdf9eedf23bc8a5824e951a5c31b9699a2f07
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-10-09 16:27:18 +0530

    case expression
    case is an expression
    case
    when ... then ...
    when ... then ...
    else ... 
    end
    
    when comparing numbers, we need not provide the name of the column otherwise its mandatory.

---
+++ caseexpression.sql
@@ -0,0 +1,15 @@
+select ProductKey, EnglishProductName, Color,
+case Color
+when 'Red' then 'RED'
+when 'Silver' then 'SILVER'
+else 'NA'
+end as Code
+from DimProduct
+
+select ProductKey, EnglishProductName, ListPrice,
+case
+when ListPrice >= 3000 then 'High'
+when ListPrice >= 2000 then 'Avg'
+else 'Low'
+end as Category
+from DimProduct
