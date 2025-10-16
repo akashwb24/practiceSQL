adf975feb9c69fd3a065da938e44436dccac6c2b
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-10-16 11:45:40 +0530

    offest
    offset skips rows from the given table
    it must be used with an ORDER BY clause
    often used with FETCH to limit how many rows are returned after skipping
    OFFSET value must be 0 or +ve integer

---
+++ offset.sql
@@ -0,0 +1,16 @@
+select * from dimproduct
+
+select * from dimproduct
+order by productkey
+offset 10 rows
+
+select * from dimproduct
+order by productkey
+offset 10 rows
+fetch first 10 rows only
+
+select * from dimproduct
+order by productkey
+offset 10 rows
+fetch next 10 rows only
+
