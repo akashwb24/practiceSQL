bb1e0895b65604c7aae3eb039c231c9d570ff8ec
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-10-16 11:10:50 +0530

    alter table
    alter table provides user to add/drop columns or change the datatype of any column

---
+++ altertablepart1_columns.sql
@@ -0,0 +1,16 @@
+select * from employees
+
+alter table employees
+add email varchar(30)
+
+alter table employees
+add mobile varchar(10), phone varchar(10)
+
+alter table employees
+drop column phone
+
+alter table employees
+drop column phone, mobile
+
+alter table employees
+alter column email varchar(50)
