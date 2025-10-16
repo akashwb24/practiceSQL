5a8170abca629735699162ad87587b37877a8922
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-10-16 11:31:07 +0530

    alter table constraints
    only that columns having defines as NOT NULL can be made PRIMARY KEY
    then on the other column FOREIGN KEY can be added
    by default DROP removes the constraints
    in order to remove the primary key, first we need to remove the foreign key then only primary key can be dropped.

---
+++ altertablepart2_constraints.sql
@@ -0,0 +1,19 @@
+create table empnew(
+empid int, 
+empname varchar(50)
+)
+
+alter table empnew
+alter column empid int not null
+
+alter table empnew
+add constraint pk_new primary key(empid)
+
+create table salaries(
+empid int,
+salary money
+)
+
+alter table salaries
+add constraint fk_new foreign key(empid)
+references empnew(empid)
