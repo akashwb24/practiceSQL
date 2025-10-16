dd15f50d2ebbc2ea4f7b32854cf901d9dbdfbc75
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-10-16 13:44:31 +0530

    schema
    schema refers to the logical structure or blueprint of a database that defines how data is organized, stored and related.
    logical schema is the abstract design focusing on entities and relationships.
    physical schema deals with how data is stored on the disk(files, storage formats)

---
+++ schema.sql
@@ -0,0 +1,15 @@
+create schema Sales
+
+create table Sales.SalesTran(
+orderid int,
+orderdate datetime,
+qty int
+)
+
+select * from Sales.SalesTran
+
+select * from SalesTran
+
+drop schema Sales
+
+drop table Sales.SalesTran
