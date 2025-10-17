2f06a2460d00ed19022a7c794c8000a89b14b059
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-10-17 17:14:01 +0530

    cte_intro
    common table expression or temporary named result set

---
+++ commontableexpressionpart1introduction.sql
@@ -0,0 +1,9 @@
+with cteselectsales
+as
+(
+select productkey, year(orderdate) orderyear, salesamount
+from factinternetsales
+)
+select productkey, orderyear, sum(salesamount) totalsales
+from cteselectsales
+group by productkey, orderyear
