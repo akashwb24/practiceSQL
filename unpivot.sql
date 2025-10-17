3c486ea21d41e0ca5360536d8273c6516947dec0
Akash P <99805268+akashwb24@users.noreply.github.com>
2025-10-17 16:12:58 +0530

    unpivot
    unpivot is the reverse of pivot operation, not fully opposite.

---
+++ unpivot.sql
@@ -0,0 +1,13 @@
+select * from employees
+
+select * into pivotdata from (select city, department, salary 
+from employees) t
+pivot(sum(salary) for department in (IT, SALES)) p
+
+select * from pivotdata
+
+select * from (select city, it, sales from pivotdata) t
+unpivot (salary for department in (it, sales)) u
+
+select city, department,salary from (select city, it, sales from pivotdata) t
+unpivot (salary for department in (it, sales)) u
