5d1fa745767e5f9b4c68b2472b6b6279eb4ad58e
Akash <99805268+akashwb24@users.noreply.github.com>
2025-12-06 23:06:31 +0530

    Update datetimefunctionspart1.sql

--- datetimefunctionspart1.sql
+++ datetimefunctionspart1.sql
@@ -5,10 +5,10 @@ select sysdatetime()
 
 select SYSUTCDATETIME()
  
+select '2023-11-31'
 
- select '2023-11-31'
-
- select datefromparts(2023,12,31)
+select datefromparts(2023,12,31)
 
 
- select datetimefromparts(2023,12,31,8,45,30,0)
+select datetimefromparts(2023,12,31,8,45,30,0)
+
