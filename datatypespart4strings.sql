5bf1a6d4bf197492558aa58f88db6c34bc00e1a2
Akash <99805268+akashwb24@users.noreply.github.com>
2025-12-06 23:06:05 +0530

    Update datatypespart4strings.sql

--- datatypespart4strings.sql
+++ datatypespart4strings.sql
@@ -1,6 +1,5 @@
 create table tblstrdatatypes(
 employeename char, city varchar
-
 )
 
 insert into tblstrdatatypes(employeename, city)
@@ -12,5 +11,6 @@ insert into tblstrdatatypes(employeename, city)
 values ('ssssss','ddddddd')
 
 create table tblstr2(names varchar(max))
+
 
 
