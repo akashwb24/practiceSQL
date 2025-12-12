9934e53448e1446bc292e79534ccdb207277c61e
Akash <99805268+akashwb24@users.noreply.github.com>
2025-12-12 22:25:31 +0530

    Update COALESCE_function.sql

--- COALESCE_function.sql
+++ COALESCE_function.sql
@@ -6,6 +6,7 @@ halfyearly money,
 quaterly money,
 monthly money
 )
+
   
 insert into vendors(vendorid,vendorname,yearly,halfyearly,quaterly,monthly)
 values(1,'xyz company',20000,null,null,null),
@@ -13,17 +14,23 @@ values(1,'xyz company',20000,null,null,null),
 (3,'door step delivery',null,null,6000,null),
 (4,'tcl telecom',null,null,null,1200)
 
+  
 select * from vendors
 
+  
 select coalesce (null, 100)
 select coalesce (null, null) --error
 select coalesce (null, 100, null, 234, 9845)
 
+  
 select vendorid, vendorname, coalesce(yearly, halfyearly, quaterly, monthly)
 from vendors
 
+  
 select vendorid, vendorname, coalesce(yearly, halfyearly*2, quaterly*4, monthly*12)
 
+  
 from vendors
+
 
 
