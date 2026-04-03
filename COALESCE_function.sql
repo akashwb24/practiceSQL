022c8e73cc31e94f7ead501bc797a6cf5c71b11d
Akash <99805268+akashwb24@users.noreply.github.com>
2026-04-03 23:30:06 +0530

    Update COALESCE_function.sql

--- COALESCE_function.sql
+++ COALESCE_function.sql
@@ -7,6 +7,7 @@ quaterly money,
 monthly money
 )
 
+  
 insert into vendors(vendorid,vendorname,yearly,halfyearly,quaterly,monthly)
 values(1,'xyz company',20000,null,null,null),
 (2,'abc express',null,10000,null,null),
