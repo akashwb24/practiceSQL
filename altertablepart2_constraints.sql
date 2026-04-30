e2a8c8796c92fd7ddeff58a62c9583ea2717c0fc
Akash <99805268+akashwb24@users.noreply.github.com>
2026-04-30 23:46:48 +0530

    Update altertablepart2_constraints.sql

--- altertablepart2_constraints.sql
+++ altertablepart2_constraints.sql
@@ -9,15 +9,12 @@ alter column empid int not null
 alter table empnew
 add constraint pk_new primary key(empid)
 
-
 create table salaries(
 empid int,
 salary money
 )
 
-
 alter table salaries
 add constraint fk_new foreign key(empid)
 
 references empnew(empid)
-
