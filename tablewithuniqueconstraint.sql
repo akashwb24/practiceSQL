cea0fab954209b75f660eb003b6ad0c4398868fa
Akash <99805268+akashwb24@users.noreply.github.com>
2026-05-04 23:27:16 +0530

    Update tablewithuniqueconstraint.sql

--- tablewithuniqueconstraint.sql
+++ tablewithuniqueconstraint.sql
@@ -19,6 +19,5 @@ values(2,'mack','2001-02-18',40000,'mack@gmail.com')
 insert into employees3 (employeeid, employeename, doj, salary, email)
 values(3,'prik','2002-07-25',35000,'prik@gmail.com')
 
-
 insert into employees3 (employeeid, employeename, doj, salary, email)
 values(4,'amit','2003-08-29',30000,null)
