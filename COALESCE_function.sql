148542d602541e34a39b9ad2d47b10eeec54afb8
Akash <99805268+akashwb24@users.noreply.github.com>
2026-06-05 22:58:16 +0530

    Update COALESCE_function.sql

--- COALESCE_function.sql
+++ COALESCE_function.sql
@@ -12,7 +12,10 @@ values(1,'xyz company',20000,null,null,null),
 (2,'abc express',null,10000,null,null),
 (3,'door step delivery',null,null,6000,null),
 (4,'tcl telecom',null,null,null,1200)
-  
+
+
+
+ 
 select * from vendors
 
 select coalesce (null, 100)
